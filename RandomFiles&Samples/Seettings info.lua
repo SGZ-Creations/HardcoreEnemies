26551445cc696f9a1bb0d50fc6ff0ed7a73166ba
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-26 12:31:47 +0200

    Moved to README

--- RandomFiles&Samples/Seettings info.lua
+++
@@ -1,5 +0,0 @@
---[[
-    The DamageMultiplier startup setting controls how much damage the biter's will do
-    The ResistanceMultiplier startup setting controls how much resistances are applied to buildings
-    The CombatResistanceMultiplier startup setting controls how much resistances combat buildings get as opposed to regular buildings
-]] --
