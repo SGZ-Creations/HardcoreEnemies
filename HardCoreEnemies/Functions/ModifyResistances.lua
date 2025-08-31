f8616fe7a2cd2518a8ef916764e35ff2110b1977
jaythom2723 <penguinking684@gmail.com>
2025-08-30 23:29:06 -0500

    removing redundant comments and code

--- HardCoreEnemies/Functions/ModifyResistances.lua
+++ HardCoreEnemies/Functions/ModifyResistances.lua
@@ -9,81 +9,6 @@
     {"combat-building", true}
 ]] --
 
--- local building_types = {
---     { "accumulator",               false },
---     { "beacon",                    false },
---     { "boiler",                    false },
---     { "arithmetic-combinator",     false },
---     { "decider-combinator",        false },
---     { "selector-combinator",       false },
---     { "constant-combinator",       false },
---     { "container",                 false },
---     { "logistic-container",        false },
---     { "infinity-container",        false },
---     { "temporary-container",       false },
---     { "assembling-machine",        false },
---     { "rocket-silo",               false },
---     { "furnace",                   false },
---     { "display-panel",             false },
---     { "electric-energy-interface", false },
---     { "electric-pole",             false },
---     { "combat-robot",              false },
---     { "construction-robot",        false },
---     { "logistic-robot",            false },
---     { "generator",                 false },
---     { "heat-interface",            false },
---     { "heat-pipe",                 false },
---     { "inserter",                  false },
---     { "lab",                       false },
---     { "lamp",                      false },
---     { "land-mine",                 false },
---     { "linked-container",          false },
---     { "market",                    false },
---     { "mining-drill",              false },
---     { "offshore-pump",             false },
---     { "pipe",                      false },
---     { "infinity-pipe",             false },
---     { "pipe-to-ground",            false },
---     { "power-switch",              false },
---     { "programmable-speaker",      false },
---     { "proxy-container",           false },
---     { "pump",                      false },
---     { "radar",                     false },
---     { "curved-rail-a",             false },
---     { "curved-rail-b",             false },
---     { "half-diagonal-rail",        false },
---     { "legacy-curved-rail",        false }, -- TODO: remove when 2.1 comes out
---     { "legacy-straight-rail",      false }, -- TODO: remove when 2.1 comes out
---     { "straight-rail",             false },
---     { "rail-chain-signal",         false },
---     { "rail-signal",               false },
---     { "reactor",                   false },
---     { "roboport",                  false },
---     { "solar-panel",               false },
---     { "storage-tank",              false },
---     { "train-stop",                false },
---     { "lane-splitter",             false },
---     { "linked-belt",               false },
---     { "splitter",                  false },
---     { "transport-belt",            false },
---     { "underground-belt",          false },
---     { "valve",                     false },
---     { "cargo-wagon",               false },
---     { "infinity-cargo-wagon",      false },
---     { "fluid-wagon",               false },
---     { "artillery-turret",          true },
---     { "ammo-turret",               true },
---     { "electric-turret",           true },
---     { "fluid-turret",              true },
---     { "spider-leg",                true },
---     { "car",                       true },
---     { "artillery-wagon",           true },
---     { "locomotive",                true },
---     { "spider-vehicle",            true },
---     { "wall",                      true },
---     { "gate",                      true },
--- }
-
 local combat_buildings = {
     "artillery-turret",
     "ammo-turret",
@@ -97,26 +22,6 @@ local combat_buildings = {
     "wall",
     "gate"
 }
-
--- if mods['space-age'] then -- add capability with space age shit
---     table.insert(building_types, { "cargo-pod", false })
---     table.insert(building_types, { "cargo-landing-pad", false })
---     table.insert(building_types, { "agricultural-tower", false })
---     table.insert(building_types, { "capture-robot", true })
---     table.insert(building_types, { "plant", false })
---     table.insert(building_types, { "cargo-bay", false })
---     --table.insert(building_types, { "space-platform-hub", false })Space object
---     --table.insert(building_types, { "thruster", false })Space object
---     --table.insert(building_types, { "asteroid-collector", false })Space object
--- end
--- if mods['elevated-rails'] then -- add capability with space age shit
---     table.insert(building_types, { "rail-support", false })
---     table.insert(building_types, { "rail-ramp", false })
---     table.insert(building_types, { "elevated-straight-rail", false })
---     table.insert(building_types, { "elevated-half-diagonal-rail", false })
---     table.insert(building_types, { "elevated-curved-rail-b", false })
---     table.insert(building_types, { "elevated-curved-rail-a", false })
--- end
 
 local AddResistances = function(resistances)
     local truth_table = {}
